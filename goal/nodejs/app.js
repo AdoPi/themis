df134972b44ddd9259bb30a5d65e4454137da3c2
Adonis Najimi <adopi.naj@gmail.com>
2018-06-19 00:37:27 +0200

    datastore added

--- goal/nodejs/app.js
+++ goal/nodejs/app.js
@@ -7,8 +7,19 @@ var path = require('path');
 
 app.use(express.static('public'));
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
+const store_data = true;
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ players: [], lb_name: 'Team' })
+  .write()
+
+//
 //var config = require('./config')
 
 /*
@@ -70,11 +81,32 @@ var players = [
 
 ];
 app.get('/players',function(req,res) {
-    res.json(players);
+    if (store_data) {
+        res.json(db.get('players'));
+    } else {
+        res.json(players);
+    }
 });
 
 app.get('/players/add/:name',function(req,res) {
     console.log('adding')
+    if (store_data) {
+        db.get('players')
+            .push({
+                "doubles_last_movement": 0,//data[key].doubles_last_movement,
+                "doubles_lost": 0, //data[key].doubles_lost,
+                "doubles_points": 0, //data[key].doubles_points,
+                "doubles_won": 0, //data[key].doubles_won,
+                "dt": 0,
+                "name": req.params.name,
+                "singles_last_movement": 0, //data[key].singles_last_movement,
+                "singles_lost": 0, //data[key].singles_lost,
+                "singles_points": 0,
+                "singles_won": 0, //data[key].singles_won,
+                "status": true //data[key].status
+            })
+        .write();
+} else {
     players.push({
         "doubles_last_movement": 0,//data[key].doubles_last_movement,
         "doubles_lost": 0, //data[key].doubles_lost,
@@ -89,7 +121,7 @@ app.get('/players/add/:name',function(req,res) {
         "status": true //data[key].status
     });
     res.json({'code':200});
-});
+}});
 
 
 app.post('/players/update/:name/', function(req,res) {
@@ -132,13 +164,29 @@ app.get('/players/score/:name/:score',function(req,res) {
 });
 
 app.get('/players/addscore/:name/:score',function(req,res) {
-    players.forEach(function(player) {
-        if (player.name == req.params.name) {
-            console.log('updated' + req.params.score);
-            player.singles_points = parseInt(player.singles_points) + parseInt(req.params.score);
-            player.points = parseInt(player.points) + parseInt(req.params.score);
-        }
-    });
+    if (store_data) {
+        const player = db.get('players')
+            .find({name: req.params.name})
+            .value();
+        var singles_points = parseInt(player.singles_points) + parseInt(req.params.score);
+        var points = parseInt(player.points) + parseInt(req.params.score);
+
+        db.get('players')
+            .find({'name': req.params.name})
+            .assign({
+                singles_points   : singles_points,
+                points : points
+            })
+            .value();
+    } else {
+        players.forEach(function(player) {
+            if (player.name == req.params.name) {
+                player.singles_points = parseInt(player.singles_points) + parseInt(req.params.score);
+                player.points = parseInt(player.points) + parseInt(req.params.score);
+            }
+        });
+    }
+
     res.json({'code':200});
 });
 
